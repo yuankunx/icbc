c804aa02a05a4912840221e070463bb269075343
icbc <18515606@qq.com>
2020-10-21 11:27:36 +0800

    工行专题页

---
+++ src/views/CrossBorder.vue
@@ -0,0 +1,28 @@
+<template>
+<div class="icbc-box">
+    <div class="icbc-title"><h1>跨境收款</h1></div>
+    <h2>1、跨境电商无票免税政策</h2>
+    <p>自2020年1月1日起，符合规定条件的跨境电商可以试行“无票免税”政策，综试区内核定征收的跨境电商企业统一采用核定应税所得率方式核定征收企业所得税；同时，综试区核定征收的跨境电商企业的应税所得率统一按照4%执行。</p>
+    <div class="icbc-img"><img src="../images/img-01.jpg" alt=""></div>
+    <p><b>政策背景</b></p>
+    <p>2018年9月，财政部、税务总局、商务部、海关总署联合发布了《关于跨境电子商务综合试验区零售出口货物税收政策的通知》（财税〔2018〕103号），对跨境电子商务综合试验区（以下简称“综试区”）内的跨境电子商务零售出口企业（以下简称“跨境电商企业”）未取得有效进货凭证的货物，凡符合规定条件的，出口免征增值税和消费税（以下简称“无票免税”政策）。</p>
+    <p>为支持跨境电商新业态发展，推动外贸模式创新，配合落实“无票免税”政策，国务院常务会议决定，出台更加便利企业的所得税核定征收办法。因此，税务总局制发《公告》，进一步明确跨境电商企业所得税核定征收有关问题，促进跨境电商企业更好开展出口业务。</p>
+    <h2>2、跨境电商跨境收款解决</h2>
+    <p>跨汇通跨境收款平台对接银行、跨境电商平台，提供全流程的跨境收款解决方案，同时协助银行拓展商户并对业务提供运营支持。</p>
+    <div class="icbc-img"><img src="../images/img-02.jpg" alt=""></div>
+    <p><b>背景</b></p>
+    <p>跨汇通是跨国通科技有限公司和工商银行合作，搭建跨境收款平台，实现跨境收款业务流程线上化，商户在跨境收款平台开通银行收款账户，确保资金始终在银行掌控，资金路径符合监管要求。资金入境由境内银行发起，所有资金经过反洗钱、反恐、防拆分、外管关注名单等筛查，并根据从跨境电商平台获取到的订单、申报等数据对资金进行逐笔还原并申报。</p>
+</div>
+</template>
+<script>
+    export default {
+
+    }
+</script>
+<style scoped>
+ .icbc-box{ width: 1190px; overflow: hidden; background-color: #fff; border-radius: 5px; margin: 30px 0; box-sizing: border-box; padding: 50px; line-height: 36px; font-size: 18px;}
+.icbc-box .icbc-img{padding: 20px 0; overflow: hidden; text-align: center;}
+.icbc-box .icbc-title{ height: 100px; text-align: center; font-size: 36px; line-height: 50px; color: #ff3c00;}
+.icbc-box h2{font-size: 20px; margin-bottom: 20px;}
+.icbc-box p{margin-bottom: 20px; text-indent: 2em;}
+</style>
---
+++ src/views/Enterprise.vue
@@ -0,0 +1,40 @@
+<template>
+<div class="icbc-box">
+    <div class="icbc-enterprise" v-for="item in enterprise" :key="item.id">
+        <div class="enterprise-box g-f-l">
+            <img :src="item.left.logo" alt="">
+            <div class="enterprise-text">
+                <h1>{{item.left.name}}</h1>
+                <p>{{item.left.text}}</p>
+            </div>
+        </div>
+        <div class="enterprise-line"></div>
+        <div class="enterprise-box g-f-r">
+            <img :src="item.right.logo" alt="">
+            <div class="enterprise-text">
+                <h1>{{item.right.name}}</h1>
+                <p>{{item.right.text}}</p>
+            </div>
+        </div>
+    </div>
+</div>
+</template>
+<script>
+    export default {
+      data(){
+        return {
+          enterprise: this.$store.state.enterprise
+        }
+      }
+    }
+</script>
+<style scoped>
+.icbc-box{margin: 30px 0;}
+.icbc-enterprise{ width: 1190px; height: 200px; background-color: #fff; margin-bottom: 20px; border-radius: 5px;}
+.icbc-enterprise .enterprise-box{width: 594px; height: 200px; box-sizing: border-box; padding: 30px;}
+.icbc-enterprise .enterprise-box img{width: 120px; height: 120px; float: left; margin-right: 20px;}
+.icbc-enterprise .enterprise-box .enterprise-text{ width: 394px; float: left;}
+.icbc-enterprise .enterprise-box .enterprise-text h1{color: #fb5b02; padding-bottom: 10px; font-size: 18px;}
+.icbc-enterprise .enterprise-box .enterprise-text p{line-height: 24px;}
+.icbc-enterprise .enterprise-line{width: 1px; height: 115px; background-color: #dadada; float: left; margin-top: 40px;}
+</style>
