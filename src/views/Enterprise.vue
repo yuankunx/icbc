e95aa6671d7d15d2a29519bb9677a5a1868eebc0
yuankunx <18515606@qq.com>
2020-10-27 17:27:50 +0800

    工行专题页

--- src/views/Enterprise.vue
+++ src/views/Enterprise.vue
@@ -30,8 +30,8 @@
 </script>
 <style scoped>
 .icbc-box{margin: 30px 0;}
-.icbc-enterprise{ width: 1190px; height: 200px; background-color: #fff; margin-bottom: 20px; border-radius: 5px;}
-.icbc-enterprise .enterprise-box{width: 594px; height: 200px; box-sizing: border-box; padding: 30px;}
+.icbc-enterprise{ width: 1190px; overflow: hidden; background-color: #fff; margin-bottom: 20px; border-radius: 5px;}
+.icbc-enterprise .enterprise-box{width: 594px; overflow: hidden; box-sizing: border-box; padding: 30px;}
 .icbc-enterprise .enterprise-box img{width: 120px; height: 120px; float: left; margin-right: 20px;}
 .icbc-enterprise .enterprise-box .enterprise-text{ width: 394px; float: left;}
 .icbc-enterprise .enterprise-box .enterprise-text h1{color: #fb5b02; padding-bottom: 10px; font-size: 18px;}
